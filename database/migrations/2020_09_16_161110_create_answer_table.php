3f777ec43b6f310d1f8562dfc257325f432987e5
Olle Källén <olle.kallen@hotmail.com>
2020-09-16 18:18:03 +0200

    add migrations

---
+++ database/migrations/2020_09_16_161110_create_answer_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnswerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answer', function (Blueprint $table) {
+            $table->id();
+            $table->string('answer');
+            $table->boolean('is_correct');
+            $table->unsignedInteger('question_id');
+            $table->unsignedInteger('game_id');
+            $table->timestamp('deadline_at');
+            $table->timestamp('submitted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answer');
+    }
+}
